a95ba5606d1bc5f96534c2655a3507fc2524f9be
Ben Grisdale <bengris32@protonmail.ch>
2026-05-31 16:43:16 +0100

    Revert "[ALPS05544046] IPv6: Report No RA after trying 3 times"
    This reverts commit 54722324a411471d67d19d6302cc2209e04dcea9.
    
    Change-Id: Id6cce60a86b1bbe106e8fcc79e2d3fa62c5530d3
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -95,7 +95,6 @@
 #include <linux/export.h>
 
 #define	INFINITY_LIFE_TIME	0xFFFFFFFF
-#define	RTR_SOLICITS_MAX	3
 
 #define IPV6_MAX_STRLEN \
 	sizeof("ffff:ffff:ffff:ffff:ffff:ffff:255.255.255.255")
@@ -3995,12 +3994,6 @@ static void addrconf_rs_timer(struct timer_list *t)
 	if (idev->if_flags & IF_RA_RCVD)
 		goto out;
 
-	if (idev->rs_probes == RTR_SOLICITS_MAX && (idev->if_flags & IF_RS_VZW_SENT)) {
-		inet6_no_ra_notify(RTM_DELADDR, idev);
-		idev->if_flags &= ~IF_RS_VZW_SENT;
-		goto out;
-	}
-
 	if (idev->rs_probes++ < idev->cnf.rtr_solicits || idev->cnf.rtr_solicits < 0) {
 		write_unlock(&idev->lock);
 		if (!ipv6_get_lladdr(dev, &lladdr, IFA_F_TENTATIVE))
@@ -4025,6 +4018,14 @@ static void addrconf_rs_timer(struct timer_list *t)
 				      idev->cnf.rtr_solicit_delay :
 				      idev->rs_interval);
 	} else {
+		inet6_no_ra_notify(RTM_DELADDR, idev);
+		if ((sysctl_optr == MTK_IPV6_VZW_ALL ||
+		     sysctl_optr == MTK_IPV6_EX_RS_INTERVAL) &&
+		    (strncmp(dev->name, "ccmni", 2) == 0)) {
+			/*add for VzW feature : remove IF_RS_VZW_SENT flag*/
+			if (idev->if_flags & IF_RS_VZW_SENT)
+				idev->if_flags &= ~IF_RS_VZW_SENT;
+		}
 		/*
 		 * Note: we do not support deprecated "all on-link"
 		 * assumption any longer.
