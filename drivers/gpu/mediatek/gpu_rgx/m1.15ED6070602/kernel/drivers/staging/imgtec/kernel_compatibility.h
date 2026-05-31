43d949b81729ef6a37d53a24a794dab59c53f209
Ben Grisdale <bengris32@protonmail.ch>
2026-05-31 19:50:19 +0100

    fixup! gpu: mediatek: gpu_rgx: Use new mmap_{write,read}_lock APIs
    The original change was actually broken and only showed up when
    we renamed mmap_sem to mmap_lock. Fix the broken version check.
    
    Test: Compile k61v1_64_bsp_defconfig
    Change-Id: Iac9c48e4ca26111ca1bd4324c283b328f7c712ae
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

--- drivers/gpu/mediatek/gpu_rgx/m1.15ED6070602/kernel/drivers/staging/imgtec/kernel_compatibility.h
+++ drivers/gpu/mediatek/gpu_rgx/m1.15ED6070602/kernel/drivers/staging/imgtec/kernel_compatibility.h
@@ -451,7 +451,7 @@ __pvr_access_ok_compat(int type, const void __user * addr, unsigned long size)
  * renamed to "mmap_lock" in v5.8. Moreover, new APIs are provided to
  * access this lock starting from v5.8.
  */
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 19, 325))
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 19, 0))
 
 #define mmap_write_lock(mm)   down_write(&mm->mmap_sem)
 #define mmap_write_unlock(mm) up_write(&mm->mmap_sem)
@@ -459,7 +459,7 @@ __pvr_access_ok_compat(int type, const void __user * addr, unsigned long size)
 #define mmap_read_lock(mm)    down_read(&mm->mmap_sem)
 #define mmap_read_unlock(mm)  up_read(&mm->mmap_sem)
 
-#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(4, 19, 325) */
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(4, 19, 0) */
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0))
 #define drm_gem_object_put(obj) drm_gem_object_unreference_unlocked(obj)
