e86628c2116f49078cdc548c26d31d7dad429019
Qun-Wei Lin <qun-wei.lin@mediatek.com>
2026-08-17 09:31:53 +0700

    mm: Add Kcompressd for accelerated memory compression [FROMLIST]
    This patch series introduces a new mechanism called kcompressd to
    improve the efficiency of memory reclaiming in the operating system.
    
    Problem:
      In the current system, the kswapd thread is responsible for both scanning
      the LRU pages and handling memory compression tasks (such as those
      involving ZSWAP/ZRAM, if enabled). This combined responsibility can lead
      to significant performance bottlenecks, especially under high memory
      pressure. The kswapd thread becomes a single point of contention, causing
      delays in memory reclaiming and overall system performance degradation.
    
    Solution:
      Introduced kcompressd to handle asynchronous compression during memory
      reclaim, improving efficiency by offloading compression tasks from
      kswapd. This allows kswapd to focus on its primary task of page reclaim
      without being burdened by the additional overhead of compression.
    
    In our handheld devices, we found that applying this mechanism under high
    memory pressure scenarios can increase the rate of pgsteal_anon per second
    by over 260% compared to the situation with only kswapd. Additionally, we
    observed a reduction of over 50% in page allocation stall occurrences,
    further demonstrating the effectiveness of kcompressd in alleviating memory
    pressure and improving system responsiveness.
    
    Co-developed-by: Barry Song <21cnbao@gmail.com>
    Signed-off-by: Barry Song <21cnbao@gmail.com>
    Signed-off-by: Qun-Wei Lin <qun-wei.lin@mediatek.com>
    Reference: Re: [PATCH 0/2] Improve Zram by separating compression context from kswapd - Barry Song
               https://lore.kernel.org/lkml/20250313093005.13998-1-21cnbao@gmail.com/
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>
    main

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6509,6 +6509,7 @@ static void __meminit pgdat_init_internals(struct pglist_data *pgdat)
 	pgdat_init_kcompactd(pgdat);
 
 	init_waitqueue_head(&pgdat->kswapd_wait);
+	init_waitqueue_head(&pgdat->kcompressd_wait);
 	init_waitqueue_head(&pgdat->pfmemalloc_wait);
 
 	pgdat_page_ext_init(pgdat);
