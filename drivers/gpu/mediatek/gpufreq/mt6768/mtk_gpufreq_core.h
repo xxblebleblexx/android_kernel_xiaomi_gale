79d027362ca3088701fc6e7187f807364039bd71
xxblebleblexx <cicaknempel2@gmail.com>
2026-09-12 00:10:09 +0700

    revert: gpufreq: set 1,02GHz as higher gpufreq
    Signed-off-by: xxblebleblexx <cicaknempel2@gmail.com>

--- drivers/gpu/mediatek/gpufreq/mt6768/mtk_gpufreq_core.h
+++ drivers/gpu/mediatek/gpufreq/mt6768/mtk_gpufreq_core.h
@@ -17,8 +17,8 @@
 /**************************************************
  * MT6768/MT6767 : GPU DVFS OPP table Setting
  **************************************************/
-#define SEG_GPU_DVFS_FREQ0			(1020000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ1			(1000000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ0			(1000000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ1			(975000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ2			(950000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ3			(925000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ4			(900000)	/* KHz */
@@ -50,8 +50,8 @@
 #define SEG_GPU_DVFS_FREQ30			(315000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ31			(299000)	/* KHz */
 
-#define SEG_GPU_DVFS_VOLT0		(97850)		/* mV x 100 */
-#define SEG_GPU_DVFS_VOLT1		(95000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT0		(95000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT1		(92500)		/* mV x 100 */
 #define SEG_GPU_DVFS_VOLT2		(90000)		/* mV x 100 */
 #define SEG_GPU_DVFS_VOLT3		(87500)		/* mV x 100 */
 #define SEG_GPU_DVFS_VOLT4		(85000)		/* mV x 100 */
