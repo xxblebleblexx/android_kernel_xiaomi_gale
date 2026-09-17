a14b0c2237903c1ec2854880dc9f811c6d0ad4ba
xxblebleblexx <cicaknempel2@gmail.com>
2026-09-17 15:28:39 +0700

    ResukiSU: add latest manual hook
    Signed-off-by: xxblebleblexx <cicaknempel2@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1728,6 +1728,15 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+    void *argv, void *envp, int *flags);
+__attribute__((hot))
+extern int ksu_handle_post_execveat(int *fd, struct filename **filename_ptr,
+    void *argv, void *envp, int *flags, int *retval);
+#endif
+
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1738,6 +1747,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+
+#ifdef CONFIG_KSU_MANUAL_HOOK
+ ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
@@ -1888,6 +1901,9 @@ out_files:
 		reset_files_struct(displaced);
 out_ret:
 	putname(filename);
+#ifdef CONFIG_KSU_MANUAL_HOOK
+ ksu_handle_post_execveat(&fd, &filename, &argv, &envp, &flags, &retval);
+#endif
 	return retval;
 }
 
